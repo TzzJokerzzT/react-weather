be81451f13c0aa088381fe4ad8a864577b87c007
Luis Rodriguez <luis.rodriguez.simancas@gmail.com>
2023-03-19 14:04:13 -0500

    Fix Enviroment

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,6 +1,6 @@
 const api = {
   key: "26ea3d6eee554af49d4194754223005",
-  path: "http://api.weatherapi.com/v1/forecast.json?key=",
+  path: "https://api.weatherapi.com/v1/forecast.json?key=",
 };
 
 export default api;
